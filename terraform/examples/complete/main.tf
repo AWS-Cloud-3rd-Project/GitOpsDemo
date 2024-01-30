a135f5881b4badf1a05c152d3763f2af028ae24a
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2024-01-31 01:08:53 +0900

    [Terraform] 테라폼 백엔드 : S3, DynamoDB
    테라폼 백엔드 로컬에서 S3와 DynamoDB로 변경 (동시 작업 방지 및 tfstate파일 공유)

--- terraform/examples/complete/main.tf
+++ terraform/examples/complete/main.tf
@@ -404,3 +404,25 @@ module "kms" {
 
   tags = local.tags
 }
+
+resource "aws_s3_bucket" "amz-draw-tfstate" {
+  bucket = "amz-draw-s3-bucket-tfstate"
+}
+
+resource "aws_s3_bucket_versioning" "amz-draw-versioning" {
+  bucket = aws_s3_bucket.amz-draw-tfstate.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-tfstate-lock"
+  hash_key       = "LockID"
+  billing_mode   = "PAY_PER_REQUEST"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- terraform/examples/complete/versions.tf
+++ terraform/examples/complete/versions.tf
@@ -11,4 +11,10 @@ terraform {
       version = ">= 2.10"
     }
   }
+    backend s3 {
+    bucket         = "amz-draw-s3-bucket-tfstate" # S3 버킷 이름
+    key            = "terraform/terraform.tfstate" # tfstate 저장 경로
+    region         = "ap-northeast-2"
+    dynamodb_table = "terraform-tfstate-lock" # dynamodb table 이름
+    }
 }
