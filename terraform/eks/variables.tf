273d7ca2db155e0a0186bff63f64dd411f256ca4
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2024-01-31 00:39:10 +0900

    [Terraform] 더미 파일 삭제

--- terraform/eks/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  description = "The name of the EKS cluster"
-  default     = "ECOMMERCE-PROD-CLUSTER-SEOUL-EKS"
-}
-
-variable "cluster_version" {
-  description = "Kubernetes version for the EKS cluster"
-  default     = "1.28"
-}
--- terraform/vpc/provider.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region = "ap-northeast-2" # 서울 리전
-  
-  # access_key = "YOUR_ACCESS_KEY"
-  # secret_key = "YOUR_SECRET_KEY"
-}
